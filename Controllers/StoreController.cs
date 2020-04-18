468ef022f4e2fee47d9fdf98a9f91a8740d9b14a
Kam5678 <kamalmundi31@gmail.com>
2020-04-18 14:34:33 -0600

    Commenting

--- Controllers/StoreController.cs
+++ Controllers/StoreController.cs
@@ -1,41 +1,60 @@
-﻿using System.Collections.Generic;
+﻿/**
+ * Authors: Tyler Lam, Gurnoor Aujila, Kamalpreet Mundi
+ * 
+ * This controller class is used to access data from our database(liquor_store database).
+ * Specifically it is used to access the store table.
+ * Retriving specific data from the table, and displaying the output on the website.
+ * 
+ * Other classes used in conjunction with this include 
+ * Context.cs
+ * StoreName.cs
+ * 
+ * 
+ * **/
+
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CPSC471_Proj.Models;
 using MySql.Data.MySqlClient;
-
 namespace CPSC471_Proj.Controllers
 {
+    // Setting up the route
+    // api/Store/...
     [Route("api/[controller]")]
     [ApiController]
-    public class StoreController : ControllerBase
+    public class StoreController : ControllerBase   // The actual controller fucntion
     {
-        private readonly Context _context;
+        private readonly Context _context;      // Initalizing variable from Context class Which decides what model class to call(other than context.cs)
 
-        public StoreController(Context context)
+        public StoreController(Context context) // Constructor
         {
             _context = context;
         }
 
         // GET: api/Store/{store_id}/name
+        // This function gets the name of the store, according to the input ID provided from the API route
+        // If a valid id is inputted, the name of the store and the store id will be displayed
         [HttpGet("{input:int}/name")]
         public async Task<IActionResult> GetStoreNameById([FromRoute] int input)
         {
-            if (!ModelState.IsValid)
+            if (!ModelState.IsValid)    // If any problem occurs  
             {
-                return BadRequest(ModelState);
+                return BadRequest(ModelState);      // Return a bad request
             }
 
+            // Get the information from the database, calling the store procedure spStoreGetNameById
+            // Providing the integer input 
             var store = await _context.StoreName.FromSql("CALL spStoreGetNameById (@id)", new MySqlParameter("@id", input)).ToListAsync();
 
-            if (store == null)
+            if (store == null)  // If null is returned(could not find in database)
             {
-                return NotFound();
+                return NotFound();  // Simply return notFound
             }
 
-            return Ok(store);
+            return Ok(store);   // Otherwise if something was found, return that variable
         }
 
 
--- Models/LiquorSalePercentage.cs
+++ Models/LiquorSalePercentage.cs
@@ -1,4 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿/**
+ * Authors: Kamalpreet Mundi, Tyler Lam, Gurnoor Aujila
+ * 
+ * This class is a model class, used to distinguish what data we need from the database. 
+ * In This case the data that will be retrieved is the liquor_id and sala_percentage
+ * from the liquor_store database. In this case we are accessing the liquor table from the database/
+ * 
+ * 
+ * 
+ * 
+ * **/
+
+
+using System.ComponentModel.DataAnnotations;
 
 namespace CPSC471_Proj.Models
 {
